f6412c90c2e08fc716484b7e6007821442fc2dd6
Santosh Pandey <Santosh Pandey>
2017-02-25 20:37:26 +0545

    extracted value from site to a list

--- scrapping.py
+++ scrapping.py
@@ -1,12 +1,16 @@
 from bs4 import BeautifulSoup
-import urllib.request
-import pandas 
-import numpy
-
-
-
+import requests
+import csv
 url ="https://cdn.rawgit.com/younginnovations/internship-challenges/master/data-analysis/scrape-it/exampledata.html"
-from urllib.request import urlopen
-html = urlopen(url)
-soup = BeautifulSoup(html)
-print (soup.prettify())
+page = requests.get(url)
+print(page.status_code)
+better = BeautifulSoup(page.content,'html.parser')
+
+table = better.find('tbody')
+
+for row in table.findAll('tr'):
+    cells=[]
+    for cell in row.findAll('td'):
+        text=cell.text
+        cells.append(text)
+        print(cells)
