8e07803137ada3342587c3681b75e0c615d7cc9a
Santosh Pandey <Santosh Pandey>
2017-02-25 22:57:37 +0545

    csv generation completed

--- run.py
+++ run.py
@@ -1,6 +1,8 @@
 from bs4 import BeautifulSoup
 import requests
 import csv
+import os
+print(os.getcwd())
 url ="https://cdn.rawgit.com/younginnovations/internship-challenges/master/data-analysis/scrape-it/exampledata.html"
 page = requests.get(url)
 print(page.status_code)
@@ -8,9 +10,17 @@ better = BeautifulSoup(page.content,'html.parser')
 
 table = better.find('tbody')
 
+rows=[]
 for row in table.findAll('tr'):
     cells=[]
     for cell in row.findAll('td'):
         text=cell.text
         cells.append(text)
-        print(cells)
+    rows.append(cells)
+
+if (os.path.isdir('./out')==False):
+    os.mkdir ("/out")
+os.chdir ("out")
+outputfile = open('./test.csv','w',newline='', encoding='utf8')
+writer = csv.writer(outputfile)
+writer.writerows(rows)
